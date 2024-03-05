f0fbd97fff649794035bafe71116a2e2a7d3ddef
Sean Bailey <73324477+SeanKuo7@users.noreply.github.com>
2024-03-05 00:46:47 -0600

    class created

--- src/pwgenerator.py
+++ src/pwgenerator.py
@@ -5,22 +5,43 @@
 
 import string
 import random
-from datetime import datetime
+# from datetime import datetime
+import time
 
-while True:
+
+class PasswordGenerator:
+    def __init__(self) -> None:
+        pass
+
+    
+    def run(self):
+        while True:
+            try:
+                currentTime = int(time.time())
+                random.seed(currentTime)
+                inputStr = input(
+                    'Input a number and a "?" (length = number & "?" add symbol:\n'
+                )
+                characters = string.ascii_letters + string.digits
+                if inputStr == "":
+                    inputStr = "32"
+                if "?" in inputStr:
+                    characters += string.punctuation
+                    inputStr = inputStr[:-1]
+                length = int(inputStr)
+                password = "".join(random.choice(characters) for x in range(length))
+                print("\n", password, "\n")
+            except Exception as e:
+                print("\nException:", e)
+
+
+def main():
     try:
-        random.seed(datetime.now())
-        inputStr = input(
-            "Input a number and a \"?\" (length = number & \"?\" add symbol:\n"
-        )
-        characters = string.ascii_letters + string.digits
-        if inputStr == "":
-            inputStr = "32"
-        if "?" in inputStr:
-            characters += string.punctuation
-            inputStr = inputStr[:-1]
-        length = int(inputStr)
-        password = "".join(random.choice(characters) for x in range(length))
-        print("\n", password, "\n")
-    except:
-        print("\nInvalid input\n")
+        generator = PasswordGenerator()
+        generator.run()
+    except Exception as e:
+        print("Error: ", e)
+
+
+if __name__ == "__main__":
+    main()
